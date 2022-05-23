7e39125f90b3fb49a30e670a2f1d7c79b6cf1ead
Yan Carlos <urbanoyaan@gmail.com>
2022-05-22 23:17:41 -0500

    New files are added

--- js/dropNav.js
+++ js/dropNav.js
@@ -2,6 +2,7 @@ const btnFirst = document.querySelector ("#firstNav")
 const listFirsy = document.querySelector ("#firstNav-list")
 const magnifyingGlass = document.querySelector ('#magnifyingGlass')
 const searchBox = document.querySelector ('#searchBox')
+const productsBtn = document.getElementById ("productsBtn")
 
 btnFirst.addEventListener('click', ()=>{
     listFirsy.classList.toggle('hidden')
@@ -10,3 +11,14 @@ btnFirst.addEventListener('click', ()=>{
 magnifyingGlass.addEventListener('click', ()=>{
     searchBox.classList.toggle('hidden')
 })
+
+function productClick() {
+    document.getElementById('productsList').classList.toggle('hidden')
+    productsBtn.classList.toggle('opacity-50')
+}
+
+function phoneBtn() {
+    document.getElementById("phoneList").classList.toggle("hidden")
+}
+
+
